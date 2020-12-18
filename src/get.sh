b93ca213f088a020613d5dd66e8504426e195274
Adrian Gonciarz <adrian.gonciarz@gmail.com>
2020-12-18 15:56:09 +0100

    drop usage of sed which is unambiguous on different platforms
    Signed-off-by: Adrian Gonciarz <adrian.gonciarz@gmail.com>

--- src/get.sh
+++ src/get.sh
@@ -2,7 +2,11 @@
 
 # Define handy functions
 get_latest_version(){
-   curl --silent "https://api.github.com/repos/keptn/keptn/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/' 
+   curl --silent "https://api.github.com/repos/keptn/keptn/releases/latest" | grep tag_name | awk 'match($0, /[0-9]+.[0-9]+.[0-9]+[.A-Za-z0-9]*/) { print substr( $0, RSTART, RLENGTH )}'
+}
+
+get_all_versions(){
+    curl --silent "https://api.github.com/repos/keptn/keptn/releases" | grep tag_name | awk 'match($0, /[0-9]+.[0-9]+.[0-9]+[.A-Za-z0-9]*/) { print substr( $0, RSTART, RLENGTH )}'
 }
 
 print_after_installation_info(){
@@ -37,15 +41,15 @@ fi
 # If KEPTN_VERSION is not provided -> automatically determine latest version 
 if [[ -z "$KEPTN_VERSION" ]]; then
     KEPTN_VERSION=$(get_latest_version)
-    printf "The newest version of Keptn is %s and will be used automatically.\n" "${KEPTN_VERSION}"
+    printf "The newest version of Keptn is %s and will be used automatically\n" "${KEPTN_VERSION}"
 else
-    AVAILABLE_VERSIONS=(`curl --silent "https://api.github.com/repos/keptn/keptn/releases" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/'`)
+    AVAILABLE_VERSIONS=(`get_all_versions`)
     if [[ ! " ${AVAILABLE_VERSIONS[@]} " =~ " ${KEPTN_VERSION} " ]]; then
         printf "Selected version %s is invalid, please make sure you use proper Keptn tag!\n" "${KEPTN_VERSION}"
         exit 1
     fi
     KEPTN_VERSION=${KEPTN_VERSION}
-    printf "We'll install specified Keptn version %s.\n" "${KEPTN_VERSION}"
+    printf "We'll install specified Keptn version %s\n" "${KEPTN_VERSION}"
 fi
 
 UNAME="$(uname)"
